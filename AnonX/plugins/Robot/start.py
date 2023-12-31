c22661bf19f491763c79702517650cd2b95aa17d
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2023-12-31 13:39:51 +0530

    Update start.py

--- AnonX/plugins/Robot/start.py
+++ AnonX/plugins/Robot/start.py
@@ -39,7 +39,7 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgUAAx0Cd5qiFAACGQVlisBV9hLWr7ZinS50sswGLjQdpgACPw4AAtTTWVRhwQ1WPesG6DME")
+            await message.reply_sticker("CAACAgUAAxkBAAELEg5lkSG_wmh9sUzzOXqtXfRxgrt7iwAC-woAAo8GaFdiFvMHu62tijQE")
             return await message.reply_photo(
                        photo=config.START_IMG_URL,
                        caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
